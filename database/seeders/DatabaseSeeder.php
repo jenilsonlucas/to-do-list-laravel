763bf96a2f72a3c32f9ede2fb11108b6e3ffb1b4
Jenilson Domingos Da Costa Lucas <jenilsonllucas@gmail.com>
2025-03-12 12:02:48 +0100

    feat(CategoryFactory): criando a factory de category
    crie a factory para poder popular o banco de dados da columan registro
    fiz isso para poder começar a trabalhar na controller onde começarei por listar
    as categorias

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,12 +13,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(10)->create();
+        //User::factory(10)->create();
 
         /*User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);*/
+
+        User::factory()
+        ->hasCategories(3)
+        ->create();
 
     }
 }
