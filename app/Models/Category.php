dee3b10e23faccc60d7413670164af88496da3c3
Jenilson Domingos Da Costa Lucas <jenilsonllucas@gmail.com>
2025-03-12 11:21:46 +0100

    feat(categoria): criando o relacionamento entre categorias e users

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    
+    /**
+     * The attributes that are mass assignable.
+     * @var list<string>
+     */
+
+     protected $fillable = [
+        'name',
+        'description',
+        'user_id'
+     ];
+
+     /**
+      * Get the user that owns the Category.
+      */
+     public function user()
+     {
+        return $this->belongsTo(User::class);
+     }
+
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -34,6 +35,15 @@ class User extends Authenticatable
     ];
 
     /**
+     * Get the Category for the User.
+     *
+     */
+    public function categories(): HasMany
+    {
+        return $this->hasMany(Category::class);
+    }
+
+    /**
      * Get the attributes that should be cast.
      *
      * @return array<string, string>
@@ -45,4 +55,5 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
 }
