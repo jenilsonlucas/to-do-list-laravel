e0ed648ddf0346a378e2ccc352cbea6e33327255
Jenilson Domingos Da Costa Lucas <jenilsonllucas@gmail.com>
2025-03-18 14:10:56 +0100

    voltando para o develop

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -28,7 +28,7 @@ class CategoryController extends Controller
 
     /**
      * Store a newly created resource in storage.
-     */
+     *
     public function store(Request $request)
     {   
                
