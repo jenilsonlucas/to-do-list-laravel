97e6bd8bd4a594e36cb2e5e486caa45d7951b1a7
jenilsonlucas <jenilsonllucas@gmail.com>
2025-04-24 13:07:32 +0100

    feat(emailVerfication): criado a verificação do email do usuario

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\TaskController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
@@ -51,3 +53,19 @@ Route::get('/registrar', [RegisterController::class, 'showRegisterForm'])->name(
 Route::post('/register', [RegisterController::class, 'register']);
 
 Route::post('/logout', [LoginController::class, 'logout']);
+
+Route::get('/email/verificar', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verificar/{id}/{hash}', function(EmailVerificationRequest $request){
+    $request->fulfill();
+
+    return redirect('/');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function(Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('message', 'Link dev verificação enviar');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
