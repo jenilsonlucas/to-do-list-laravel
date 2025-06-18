acea17e6f29876470331616903590c22af6a9208
jenilson lucas <jenilsonllucas@gmail.com>
2025-06-18 11:46:47 +0100

    fix(category): ajustando o cometário

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -30,7 +30,9 @@ class CategoryController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-    public function store(Request $request)
+    */
+    
+     public function store(Request $request)
     {   
                
         $validated = $request->validate([
